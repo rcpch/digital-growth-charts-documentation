81cddefdcc819eee6d33bc5ded69338e298d60d6
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-30 22:05:24 +0100

    Delete unused file

--- docs/_assets/_javascripts/swagger.js
+++
@@ -1,6 +0,0 @@
-import swagger from "https://esm.sh/swagger-ui-dist@5";
-
-swagger.SwaggerUIBundle({
-    url: 'https://raw.githubusercontent.com/rcpch/digital-growth-charts-server/live/openapi.json',
-    dom_id: '#swagger-ui',
-});
